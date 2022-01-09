bc7772674ca879c5e9c65d48e15af5bf078e027e
Marta Bento <marta.moreira1@gmail.com>
2022-01-09 21:07:52 +0000

    working on some css changes to fix footer and fix the page height

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,9 @@
   <!-- if the browser is not loading the data -->
   <main v-if="!loading">Data</main>
 
-  <!-- if the browser is loading the data, show an animated gif -->
+  <!-- if the browser is loading the data, show an animated loading bar to the user" -->
   <main v-else>
-    <p>Information is loading...</p>
+    <p class="text-4xl text-center font-medium leading-tight mt-8 mb-8">Information is loading...</p>
     <Loader />
   </main>
 </template>
