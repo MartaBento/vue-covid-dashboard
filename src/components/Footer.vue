bff6c3f09a4b3c74b74dc215c924d6fcb2ce036c
Marta Bento <marta.moreira1@gmail.com>
2022-01-09 19:11:41 +0000

    added footer & header to components folder

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer>
+        <div>
+
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <div></div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
