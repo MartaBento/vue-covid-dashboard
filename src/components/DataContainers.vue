2f21cbcab9f052ec4ba1f43d69227e1108056e40
Marta Bento <marta.moreira1@gmail.com>
2022-01-09 23:34:49 +0000

    working on column configuration & data

--- src/components/DataContainers.vue
+++ src/components/DataContainers.vue
@@ -1,5 +1,81 @@
 <template>
-  <div class="container"></div>
+  <div
+    class="
+      flex flex-wrap
+      -mx-3
+      overflow-hidden
+      sm:-mx-4
+      md:-mx-4
+      lg:-mx-3
+      xl:-mx-2
+      text-center
+      m-8
+    "
+  >
+    <div
+      class="
+        bg-cloud-burst-400
+        my-3
+        px-3
+        w-full
+        overflow-hidden
+        sm:my-4 sm:px-4 sm:w-1/2
+        md:my-4 md:px-4 md:w-1/2
+        lg:my-3 lg:px-3 lg:w-1/4
+        xl:my-2 xl:px-3 xl:w-1/4
+      "
+    >
+      <div class="text-cloud-burst-50">Column 1</div>
+    </div>
+
+    <div
+      class="
+        bg-cloud-burst-400
+        my-3
+        px-3
+        w-full
+        overflow-hidden
+        sm:my-4 sm:px-4 sm:w-1/2
+        md:my-4 md:px-4 md:w-1/2
+        lg:my-3 lg:px-3 lg:w-1/4
+        xl:my-2 xl:px-3 xl:w-1/4
+      "
+    >
+      <div class="text-cloud-burst-50">Column 1</div>
+    </div>
+
+    <div
+      class="
+        bg-cloud-burst-400
+        my-3
+        px-3
+        w-full
+        overflow-hidden
+        sm:my-4 sm:px-4 sm:w-1/2
+        md:my-4 md:px-4 md:w-1/2
+        lg:my-3 lg:px-3 lg:w-1/4
+        xl:my-2 xl:px-3 xl:w-1/4
+      "
+    >
+      <div class="text-cloud-burst-50">Column 1</div>
+    </div>
+
+    <div
+      class="
+        bg-cloud-burst-400
+        my-3
+        px-3
+        w-full
+        overflow-hidden
+        sm:my-4 sm:px-4 sm:w-1/2
+        md:my-4 md:px-4 md:w-1/2
+        lg:my-3 lg:px-3 lg:w-1/4
+        xl:my-2 xl:px-3 xl:w-1/4
+      "
+    >
+      <div class="text-cloud-burst-50">Column 1</div>
+    </div>
+  </div>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,28 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cloud-burst': {
+          '50': '#f8fafc',
+          '100': '#f1f5f9',
+          '200': '#e0e9f2',
+          '300': '#c4d5e8',
+          '400': '#80a2cc',
+          '500': '#4471ab',
+          '600': '#32537e',
+          '700': '#243c5a',
+          '800': '#192940',
+          '900': '#101929',
+        }
+      }
+    },
     container: {
       center: true
     },
     fontFamily: {
       'sans': ['Niramit', 'Sans-serif']
-    }
+    },
   },
   variants: {
     extend: {},
