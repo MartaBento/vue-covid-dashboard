d44ea3a4a79edd82e4f11c341020273fad9d4fc3
Marta Bento <marta.moreira1@gmail.com>
2022-01-10 00:13:24 +0000

    working on data containers, already added both stats for cases & deaths

--- src/components/DataContainers.vue
+++ src/components/DataContainers.vue
@@ -8,7 +8,6 @@
       md:-mx-4
       lg:-mx-3
       xl:-mx-2
-      text-center
       m-8
     "
   >
@@ -23,6 +22,60 @@
         md:my-4 md:px-4 md:w-1/2
         lg:my-3 lg:px-3 lg:w-1/4
         xl:my-2 xl:px-3 xl:w-1/4
+        rounded-md
+      "
+    >
+      <div class="px-6 mb-8">
+        <p class="text-2xl font-extrabold mt-8 mb-2">Cases</p>
+        <p class="text-lg font-light mb-1 text-cloud-burst-50">New</p>
+        <p class="text-5xl font-thin mb-1">
+          {{ stats.NewConfirmed }}
+        </p>
+        <p class="text-lg mb-1 text-cloud-burst-50">Total</p>
+        <p class="text-5xl font-thin mb-1">
+          {{ stats.TotalConfirmed }}
+        </p>
+      </div>
+    </div>
+    <div
+      class="
+        bg-cloud-burst-400
+        my-3
+        px-3
+        w-full
+        overflow-hidden
+        sm:my-4 sm:px-4 sm:w-1/2
+        md:my-4 md:px-4 md:w-1/2
+        lg:my-3 lg:px-3 lg:w-1/4
+        xl:my-2 xl:px-3 xl:w-1/4
+        rounded-md
+      "
+    >
+      <div class="px-6 mb-8">
+        <p class="text-2xl font-extrabold mt-8 mb-2">Deaths</p>
+        <p class="text-lg font-light mb-1 text-cloud-burst-50">New</p>
+        <p class="text-5xl font-thin mb-1">
+          {{ stats.NewDeaths }}
+        </p>
+        <p class="text-lg mb-1 text-cloud-burst-50">Total</p>
+        <p class="text-5xl font-thin mb-1">
+          {{ stats.TotalDeaths }}
+        </p>
+      </div>
+    </div>
+
+    <div
+      class="
+        bg-cloud-burst-400
+        my-3
+        px-3
+        w-full
+        overflow-hidden
+        sm:my-4 sm:px-4 sm:w-1/2
+        md:my-4 md:px-4 md:w-1/2
+        lg:my-3 lg:px-3 lg:w-1/4
+        xl:my-2 xl:px-3 xl:w-1/4
+        rounded-md
       "
     >
       <div class="text-cloud-burst-50">Column 1</div>
@@ -39,38 +92,7 @@
         md:my-4 md:px-4 md:w-1/2
         lg:my-3 lg:px-3 lg:w-1/4
         xl:my-2 xl:px-3 xl:w-1/4
-      "
-    >
-      <div class="text-cloud-burst-50">Column 1</div>
-    </div>
-
-    <div
-      class="
-        bg-cloud-burst-400
-        my-3
-        px-3
-        w-full
-        overflow-hidden
-        sm:my-4 sm:px-4 sm:w-1/2
-        md:my-4 md:px-4 md:w-1/2
-        lg:my-3 lg:px-3 lg:w-1/4
-        xl:my-2 xl:px-3 xl:w-1/4
-      "
-    >
-      <div class="text-cloud-burst-50">Column 1</div>
-    </div>
-
-    <div
-      class="
-        bg-cloud-burst-400
-        my-3
-        px-3
-        w-full
-        overflow-hidden
-        sm:my-4 sm:px-4 sm:w-1/2
-        md:my-4 md:px-4 md:w-1/2
-        lg:my-3 lg:px-3 lg:w-1/4
-        xl:my-2 xl:px-3 xl:w-1/4
+        rounded-md
       "
     >
       <div class="text-cloud-burst-50">Column 1</div>
