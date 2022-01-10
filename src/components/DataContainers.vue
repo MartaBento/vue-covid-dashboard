5caacd572c3b1e7ed4f5cd73077f01d20f6252d8
Marta Bento <marta.moreira1@gmail.com>
2022-01-10 18:46:57 +0000

    re-factored the column layout

--- src/components/DataContainers.vue
+++ src/components/DataContainers.vue
@@ -1,38 +1,159 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
-    <div class="flex justify-center text-6xl rounded-xl p-6 bg-cloud-burst-200">
-      <div class="px-6 mb-8">
-        <p class="text-2xl font-extrabold mt-8 mb-2">Cases</p>
-        <p class="text-lg font-light mb-1 text-cloud-burst-50">New</p>
-        <p class="text-5xl font-thin mb-1">
-          {{ stats.NewConfirmed }}
-        </p>
-        <p class="text-lg mb-1 text-cloud-burst-50">Total</p>
-        <p class="text-5xl font-thin mb-1">
-          {{ stats.TotalConfirmed }}
-        </p>
-      </div>
-    </div>
-    <div class="flex justify-center text-6xl rounded-xl p-6 bg-cloud-burst-200">
-      <div class="px-6 mb-8">
-        <p class="text-2xl font-extrabold mt-8 mb-2">Deaths</p>
-        <p class="text-lg font-light mb-1 text-cloud-burst-50">New</p>
-        <p class="text-5xl font-thin mb-1">
-          {{ stats.NewDeaths }}
-        </p>
-        <p class="text-lg mb-1 text-cloud-burst-50">Total</p>
-        <p class="text-5xl font-thin mb-1">
-          {{ stats.TotalDeaths }}
-        </p>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+    <div class="container px-4 mx-auto">
+      <div class="flex flex-wrap">
+        <div class="w-full p-4">
+          <div class="bg-white shadow rounded overflow-hidden">
+            <div class="pt-6 px-6 mb-10 flex justify-between items-center">
+              <span
+                class="
+                  inline-flex
+                  items-center
+                  justify-center
+                  w-9
+                  h-10
+                  bg-gray-50
+                  rounded
+                "
+              >
+                <i class="fas fa-virus"></i>
+              </span>
+              <a
+                class="
+                  py-1
+                  px-2
+                  bg-blue-50
+                  text-xs text-indigo-500
+                  rounded-full
+                "
+                href="#"
+                >Today</a
+              >
+            </div>
+            <div class="px-6 mb-6">
+              <h4 class="text-xl font-bold">{{ stats.NewConfirmed }}</h4>
+              <p class="text-xs text-gray-500">New Cases</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="flex justify-center text-6xl rounded-xl p-6 bg-cloud-burst-200">
-      <div class="text-cloud-burst-50">Column 1</div>
+    <div class="container px-4 mx-auto">
+      <div class="flex flex-wrap">
+        <div class="w-full p-4">
+          <div class="bg-white shadow rounded overflow-hidden">
+            <div class="pt-6 px-6 mb-10 flex justify-between items-center">
+              <span
+                class="
+                  inline-flex
+                  items-center
+                  justify-center
+                  w-9
+                  h-10
+                  bg-gray-50
+                  rounded
+                "
+              >
+                <i class="fas fa-virus"></i>
+              </span>
+              <a
+                class="
+                  py-1
+                  px-2
+                  bg-blue-50
+                  text-xs text-indigo-500
+                  rounded-full
+                "
+                href="#"
+                >Total</a
+              >
+            </div>
+            <div class="px-6 mb-6">
+              <h4 class="text-xl font-bold">{{ stats.TotalConfirmed }}</h4>
+              <p class="text-xs text-gray-500">Total Cases</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <div class="flex justify-center text-6xl rounded-xl p-6 bg-cloud-burst-200">
-      <div class="text-cloud-burst-50">Column 1</div>
+    <div class="container px-4 mx-auto">
+      <div class="flex flex-wrap">
+        <div class="w-full p-4">
+          <div class="bg-white shadow rounded overflow-hidden">
+            <div class="pt-6 px-6 mb-10 flex justify-between items-center">
+              <span
+                class="
+                  inline-flex
+                  items-center
+                  justify-center
+                  w-9
+                  h-10
+                  bg-gray-50
+                  rounded
+                "
+              >
+                <i class="fas fa-skull-crossbones"></i>
+              </span>
+              <a
+                class="
+                  py-1
+                  px-2
+                  bg-blue-50
+                  text-xs text-indigo-500
+                  rounded-full
+                "
+                href="#"
+                >Today</a
+              >
+            </div>
+            <div class="px-6 mb-6">
+              <h4 class="text-xl font-bold">{{ stats.NewDeaths }}</h4>
+              <p class="text-xs text-gray-500">New Deaths</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container px-4 mx-auto">
+      <div class="flex flex-wrap">
+        <div class="w-full p-4">
+          <div class="bg-white shadow rounded overflow-hidden">
+            <div class="pt-6 px-6 mb-10 flex justify-between items-center">
+              <span
+                class="
+                  inline-flex
+                  items-center
+                  justify-center
+                  w-9
+                  h-10
+                  bg-gray-50
+                  rounded
+                "
+              >
+                <i class="fas fa-skull-crossbones"></i>
+              </span>
+              <a
+                class="
+                  py-1
+                  px-2
+                  bg-blue-50
+                  text-xs text-indigo-500
+                  rounded-full
+                "
+                href="#"
+                >Total</a
+              >
+            </div>
+            <div class="px-6 mb-6">
+              <h4 class="text-xl font-bold">{{ stats.TotalDeaths }}</h4>
+              <p class="text-xs text-gray-500">Total Deaths</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
